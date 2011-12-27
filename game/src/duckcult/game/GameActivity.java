2277d6e49f36882046814f5baad26cd7d363598c
Erik Harpstead <eharpste@cs.cmu.edu>
2011-12-26 20:45:18 -0600

    added the basic structure of the game loop to the project

--- game/src/duckcult/game/GameActivity.java
+++ game/src/duckcult/game/GameActivity.java
@@ -2,12 +2,31 @@ package duckcult.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameActivity extends Activity {
-    /** Called when the activity is first created. */
+   
+	private static final String TAG = GameActivity.class.getSimpleName();
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+        Log.d(TAG, "View Added");
+    }
+    
+    protected void onDestroy() {
+    	Log.d(TAG,"Destroying...");
+    	super.onDestroy();
+    }
+    
+    protected void onStop() {
+    	Log.d(TAG,"Stopping...");
+    	super.onStop();
     }
 }
